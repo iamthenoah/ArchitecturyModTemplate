dbc158c1e310dcc601fcffe9edbb8c539985f442
Noah <56337775+iamthenoah@users.noreply.github.com>
2025-12-21 04:56:51 +0100

    Update MyExampleModFabricClient.java

--- fabric/src/main/java/com/example/fabric/client/MyExampleModFabricClient.java
+++ fabric/src/main/java/com/example/fabric/client/MyExampleModFabricClient.java
@@ -1,10 +1,7 @@
 package com.example.fabric.client;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.api.EnvType;
-import net.fabricmc.api.Environment;
 
-@Environment(EnvType.CLIENT)
 public final class MyExampleModFabricClient implements ClientModInitializer {
 
     @Override
